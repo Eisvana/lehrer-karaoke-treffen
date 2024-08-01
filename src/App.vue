705d86458e3e6c683c5f7177f33a93ae497597b0
Lenni009 <guentherhanslphdyt@gmail.com>
2024-08-01 19:57:37 +0200

    initial app functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,66 @@
 <script setup lang="ts">
-console.log('Hello Eisvana!');
+import { ref } from 'vue';
+
+const name = ref('');
+
+const webhook: string = atob(import.meta.env.VITE_DISCORD_WEBHOOK ?? '');
+
+async function sendWebhook(appears: boolean) {
+  await fetch(webhook, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: `${name.value} erscheint${appears ? '' : ' nicht'}.`,
+    }),
+  });
+}
 </script>
 
 <template>
-  <div>Hello Eisvana!</div>
+  <h1>Hello Gregor!</h1>
+  <p class="me">Ich,</p>
+  <input
+    v-model="name"
+    placeholder="Name"
+    type="text"
+  />
+  <div class="button-wrapper">
+    <button
+      :disabled="!name"
+      @click="sendWebhook(true)"
+    >
+      erscheine 🦈
+    </button>
+    <button
+      :disabled="!name"
+      class="secondary"
+      @click="sendWebhook(false)"
+    >
+      erscheine nicht
+    </button>
+  </div>
 </template>
+
+<style scoped>
+.secondary:not(:hover) {
+  background-color: #647378;
+}
+
+h1,
+.me {
+  text-align: center;
+}
+
+h1 {
+  margin-block-end: 2rem;
+}
+
+.button-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  justify-content: center;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app-template/',
+  base: '/lehrer-karaoke-treffen/',
   plugins: [vue()],
   resolve: {
     alias: {
